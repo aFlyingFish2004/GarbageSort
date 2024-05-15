1a725e86466c533878e0c2516b5709f648d0f8e5
aFlyingFish2004 <872122756@qq.com>
2024-05-15 22:05:44 +0800

    配置https环境

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
-
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: '10.122.210.57',
+    port: 5173,
+    https: {
+      key: fs.readFileSync('./certificates/private.key'),
+      cert: fs.readFileSync('./certificates/certificate.crt'),
     }
   }
 })
